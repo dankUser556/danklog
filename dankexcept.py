13ec95827187f74745ca429373aea19e12e04298
dankUser556 <dperez388@gmail.com>
2024-08-06 08:33:26 -0500

    basic layout set

---
+++ dankexcept.py
@@ -0,0 +1,26 @@
+ # dankexcept module
+
+from time import time
+def test() -> None:
+    print("Test function from Module: dankexcept")
+
+    return(0);
+
+class Logger:
+    def __init__(self):
+        self.date=self.getDate();
+    def getDate();
+        return(date)
+    def log_action(self, func_name: str, prog_name: str) -> int:
+        # 
+        return(0);
+
+
+def handle_exception(fatal: bool, func_name: str, msg: str) -> int:
+
+    return(0);
+
+if __name__ == '__main__':
+    print("dankexcept run as main");
+
+print("dankexcept global end");
---
+++ main.py
@@ -0,0 +1,10 @@
+#!/home/dank/Projects/testing/danklog/.env/bin/python3
+
+import dankexcept
+
+def main() -> None:
+    dankexcept.test();
+    print("From main()")
+
+if __name__ == '__main__':
+    exit(main());
